2a3eab6d64b1774e8115a8e3b3e1e5aead9e7b86
arjunmanevannan <talk2rjun@gmail.com>
2020-05-04 23:27:58 -0400

    Added helmet

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
+app.use(helmet());
 
 const routes = require('./routers.js')
 const connectionRoutes = require('./connectionRouters.js')
